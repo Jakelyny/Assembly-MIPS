997a3d80d6d37522ed792c97b0c3bbc87816df35
Jakelyny <jakelynysousa.a@gmail.com>
2021-09-26 11:08:34 -0300

    .

--- Final/01.s
+++ Final/01.s
@@ -3,20 +3,19 @@ vetin: .word 12,14,2,7,5,0,10,19,34,52,26,12,53,78,56,32,33,45,44,18
 vet: .space 20*4		
 spc: .asciiz ", "
 msg1: .asciiz "\nInsira um numero: " 
-msg2: .asciiz "\nOPS, O NUMERO JÁ EXISTE! ! !" 
+msg2: .asciiz "\nOPS, O NUMERO EH IGUAL! ! !\n" 
     .text
 
     .globl main
 
 main: 
-        N_VET=20*4 		
-        PRINT_INT=1		
-        PRINT_STR=4
+	N_VET=20*4 		
+	PRINT_INT=1		
+	PRINT_STR=4
 
-        li $t0, 0 		
-
+	li $t0, 0 		
 WRITE_LOOP:
-        bge $t0, N_VET, END_WRITE 
+	bge $t0, N_VET, END_WRITE 
         lw $t1, vetin($t0)
         li $t5, 0
 
@@ -26,6 +25,7 @@ TESTE_LOOP:
 
         beq $t1, $t8, IGUAL
         addi $t5, $t5, 4	
+        j TESTE_LOOP
 
 END_TESTE:
         sw $t1, vet($t0)			
@@ -34,19 +34,19 @@ END_TESTE:
 
 IGUAL:
         li $v0, 4      
-        la $a0, msg2    
+        la $a0, msg2   
         syscall
 
-        li $v0, 1    
-        move $a0, $t1   
-        syscall       
+        li $v0, 1     
+        move $a0, $t1  
+        syscall        
 
         li $v0, 4       
         la $a0, msg1   
-        syscall        
+        syscall       
 
         li $v0, 5      
-        syscall         
+        syscall       
         move $t1, $v0	
 
         sw $t1, vetin($t0)
@@ -56,17 +56,18 @@ END_WRITE:
         li $t0, 0 
 
 PRINT_LOOP:
-        lw $a0, vet($t0)	
-        li $v0, PRINT_INT   
-        syscall         	
+	lw $a0, vet($t0)		
+	li $v0, PRINT_INT  
+	syscall         	
 
-        li $v0, PRINT_STR	
-        la $a0, spc    		 
-        syscall         	
+	li $v0, PRINT_STR	
+	la $a0, spc    		 
+	     	
 
-        bge $t0, N_VET-4, END_PRINT 
-        addi $t0, $t0, 4	
-        addi $t9, $t9, 4		
-        j PRINT_LOOP
+	bge $t0, N_VET-4, END_PRINT 	
+	addi $t0, $t0, 4		
+	addi $t9, $t9, 4		
+        syscall 
+	j PRINT_LOOP
 
 END_PRINT:
